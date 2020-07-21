cf1f90d75be6af1380316765bd8c0b130230bcec
Vasu <62899114+Vasavarajan@users.noreply.github.com>
2020-07-21 23:10:20 +0530

    Add files via upload

---
+++ Day2/Assignment.py
@@ -0,0 +1,39 @@
+#Assignment 
+
+print("The backslash '\' is a special character, also called the 'escape' character.It is used in representing certain whitespace characters: '\t' is a tab, ''\n' is a newline, and '\r' is a carriage return. Conversely, prefixing a special character with '\' turns it into an ordinary character.")
+print ("Hello, World!")
+print ("hello\n world")
+print ("hello\t world")
+print ("""hello""world""")
+
+print("""Triple quotes can span multiple lines of text""") 
+print("Output formatting:")
+print("The '%' operator can also be used for string formatting, also to use formatted string literals, begin a string with f or F before the opening quotation mark or triple quotation mark can be used")
+print("Hikes : % 3d, Portal : % 5.2f %(1, 05.333)")
+
+print('Variables:')
+print("variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing.Different data types in Python are Numbers, List, Tuple, Strings, Dictionary, etc. Variables can be declared by any name or even alphabets like a, aa, abc, etc.") 
+
+
+print("Arithmetic operators:")
+print("Arithmetic operators used in python are +(Addition), -(Subtraction),*(Multiplication), /(Division), % (Modulus), ** (Exponent), //(Floor Division)")
+a = 8 
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+print('Bitwise operators:')
+print("Bitwise operators used in python are |(Bitwise OR operator), &((Bitwise AND operator), ~(Binary one's Complement), ^(Bitwise XOR operator), <<(Binary Left shift operator), >>(Binary Right shift operator)")
+print (a & b)
+print (a | b)
+print (a ^ b)
+
+
+print("Operators precedence:") 
+print(" Operators precedence in python is followed by highest precedening operator **(Exponentiation) followed by ~ + -(Complement, unary plus and minus) followed by * / % //(Multiply, divide, modulo and floor division) followed by + -(Addition and subtraction) followed by >> <<(Right and left bitwise shift) followed by &(Bitwise 'AND') and ^ |(Bitwise `XOR' and `OR') and <= < > >=(Comparison operators) and <> == !=(Equality operators)")
+ 
